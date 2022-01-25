b0720004b390fe3cc06b1de85d858bbf97e849bf
Mark Wlodawski <expresso@expresso.lan>
2022-01-24 22:10:23 -0500

    Using React context and useState for cart manipulation

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CartCSS from "./Cart.module.css";
 import { FiShoppingCart } from "react-icons/fi";
+import { AppStateContext } from "./AppState";
 
 interface Props {}
 
@@ -16,31 +17,48 @@ class Cart extends React.Component<Props, State> {
     };
   }
 
+  handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log(e.target);
+    // if ((e.target as HTMLElement).nodeName === 'SPAN') {
+    //     (e.target as HTMLSpanElement).;
+    // }
+    this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
+  };
+
   render() {
     return (
-      <div className={CartCSS.cartContainer}>
-        <button
-          type="button"
-          className={CartCSS.button}
-          onClick={() => {
-            this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
-          }}
-        >
-          <FiShoppingCart />
-          <span>2 pizza(s)</span>
-        </button>
-        <div
-          className={CartCSS.cartDropDown}
-          style={{
-            display: this.state.isOpen ? "block" : "none",
-          }}
-        >
-          <ul>
-            <li>Napoletana</li>
-            <li>Marinara</li>
-          </ul>
-        </div>
-      </div>
+      <AppStateContext.Consumer>
+        {(state) => {
+          return (
+            <div className={CartCSS.cartContainer}>
+              <button
+                type="button"
+                className={CartCSS.button}
+                onClick={this.handleClick}
+              >
+                <FiShoppingCart />
+                <span>{state.cart.items.length} pizza(s)</span>
+              </button>
+              <div
+                className={CartCSS.cartDropDown}
+                style={{
+                  display: this.state.isOpen ? "block" : "none",
+                }}
+              >
+                <ul>
+                  {state.cart.items.map((item) => {
+                    return (
+                      <li key={item.id}>
+                        {item.name} &times; {item.quantity}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          );
+        }}
+      </AppStateContext.Consumer>
     );
   }
 }
